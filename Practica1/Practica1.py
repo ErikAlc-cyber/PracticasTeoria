f83e63f7f1300330f6cce2ea6e8f55c35afd1fd3
GlitchBoy <secureerik@tutanota.com>
2022-02-22 13:17:04 -0600

    Trying to accelerate Prac1
    Debugging some stuff to make it faster

--- Practica1/Practica1.py
+++ Practica1/Practica1.py
@@ -1,9 +1,8 @@
 import math
 import random
 import matplotlib.pyplot as plt
-import os
 import time
-from os import remove
+import sys
 
 class Combinacion:
     def __init__(self, combinacion, numero_de_1, largo):
@@ -17,91 +16,55 @@ def contador(numero):
         unos += int(i)
     return unos
  
+def longitud(string):
+    lon = 0
+    for i in string:
+        if i != "\n" or i != "\0":
+            lon += 1
+    return lon
+ 
 def bina(decimal):
         
-    binario = 0
-    mult = 1
+    binario = str(bin(decimal))[2:]
+    unos = contador(binario)
+    
+    return Combinacion(binario, unos, longitud(binario))
 
-    while decimal != 0:
-        binario = binario + decimal % 2 * mult
-        decimal //= 2
-        mult *= 10
+def ingerir(unos, largo):
     
-    strbinario = str(binario)
-    unos = contador(strbinario)
+    graficar(unos, "Combinaciones posibles", "# de unos")
+    graficar(largo, "Combinaciones posibles", "# de digitos")
     
-    return Combinacion(binario, unos, len(strbinario))
-
-def primo(numero):
-    if numero == 4:
-        return False
-    for x in range(2, int(numero/2)):
-        if numero % x == 0:
-            return False
-    return True
-
-def cleartext(out_file, file):
-    array=[]
-    in_file=open(file, "r")
-    while True:
-        file_line = in_file.readline()
-        if not file_line:
-            in_file.close() #cierro el archivo
-            break
-        array.append(file_line)
-        out_file.write(","+file_line.strip('\n'))
-    os.remove(file)
-    return array
-
-def ingerir():
-    
-    out_file = open("resultados.txt", "w") #Abre el txt 
-    out_file.write("L{e") #escribo info en el txt
-    
-    combinaciones=cleartext(out_file, "combinaciones.txt")
-    
-    out_file.write("}\n")
-    out_file.write("#1's{e")
-    
-    unos=cleartext(out_file, "unos.txt")
-    
-    out_file.write("}\n")
-    out_file.write("# de Digitos{e")
-    
-    largo=cleartext(out_file, "largo.txt")
-    
-    out_file.write("}\n")
-    out_file.close() 
-
-    #graficar(combinaciones, unos, "Combinaciones posibles", "# de unos")
-    #graficar(combinaciones, largo, "Combinaciones posibles", "# de digitos")
-    
-    #graficar(combinaciones, logrec(unos), "Combinaciones posibles log10", "# de unos")
-    #graficar(combinaciones, logrec(largo), "Combinaciones posibles log10", "# de digitos")
+    graficar(logrec(unos), "Combinaciones posibles log10", "# de unos")
+    graficar(logrec(largo), "Combinaciones posibles log10", "# de digitos")
     
 def logrec(array):
     aux=[]
     for i in array:
-        aux.append(math.log10(int(i)))
+        if i == 0:
+            pass
+        else:
+            aux.append(math.log10(int(i)))
     return aux
     
-def graficar(x_axis, y_axis, titlex, titley):
+def graficar(y_axis, titlex, titley):
     try:
         inicio = time.time()
-        fig, ax = plt.subplots()
-        ax.set_ylabel(titley)
-        ax.set_title(titlex)
-        ax.grid(False);
-        ax.plot(x_axis,y_axis)
-        ax.autoscale(enable=True, axis='both', tight=None)
-        plt.show()
+        ax = plt.subplot()
+        plt.xlabel(titlex)
+        ax.set_yscale('log')
+        plt.ylabel(titley)
+        plt.title(titlex + " y "+titley)
+        ax.plot(y_axis)
         fin = time.time()
+        plt.show()
         print("El tiempo de ejecucion para la grafica: "+titlex+", "+titley+" es: "+str(fin-inicio))
         
     except (KeyboardInterrupt, BufferError):
         fin = time.time()
+        plt.show()
         print("El tiempo de ejecucion para la grafica: "+titlex+", "+titley+" es: "+str(fin-inicio))
-        pass
+        
 
 if __name__ == "__main__":
     while True:
@@ -122,56 +85,45 @@ if __name__ == "__main__":
                     break
             
             elif aux ==2:
-                n = random.randrange(0,1000)
+                n = random.randint(1,1000)
             
             else:
                 print("Error, seleccione una opcion correcta")
                 break
             
-            check = True
-            count = 1
+            lim = pow(2, n)
+            unos = []
+            largo = []
             
-            filec = open("combinaciones.txt", "w")
-            fileu = open("unos.txt", "w")
-            filel = open("largo.txt", "w")
+            filec = open("resultados.txt", "w") 
             filep = open("primos.txt", "w")
+            filec.write('Σ^* = { ε, \n')
             
             inicio = time.time()
-            while check:
+            for i in range (lim):
                 try:
-                    cadena = bina(count)
-                    if(primo(count)):
-                        filep.write(str(cadena.numero)+"--"+ str(count)+" \n")
-                    filec.write(str(cadena.numero)+"\n")
-                    fileu.write(str(cadena.numero_de_1)+"\n")
-                    filel.write(str(cadena.largo)+"\n")
-                    count += 1
-                    check = cadena.numero_de_1 != n
+                    cadena = bina(i)
+                    filec.write(str(cadena.numero)+",\n")
+                    unos.append(cadena.numero_de_1)
+                    largo.append(cadena.largo)
                     del cadena
                     
-                except BufferError:
-                    filec.close()
-                    fileu.close()
-                    filel.close()
-                    filep.close()
-                    exit()
-                    
-                except KeyboardInterrupt:
+                except (KeyboardInterrupt, BufferError):
                     break
 
+            filec.write("}")
             fin = time.time()
             filec.close()
-            fileu.close()
-            filel.close()
             filep.close()
+            print(sys.getsizeof(unos))
+            print(sys.getsizeof(largo))
             print("El tiempo de ejecucion para n = "+str(n)+" es: "+str(fin-inicio))
             del inicio
             del fin
             del aux
             del opc
             del n
-            del count
-            ingerir()
+            ingerir(unos, largo)
             
             
         elif opc == 2:
