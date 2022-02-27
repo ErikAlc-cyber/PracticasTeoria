daf9b14618ede13d8da27805ad763f70e81e752c
Erik Alc <ealcantarac1700@alumno.ipn.mx>
2022-02-27 14:03:26 -0600

    Practica2 V1 iteration

--- Practica2/Practica2.py
+++ Practica2/Practica2.py
@@ -38,7 +38,27 @@ def primo(numero):
         if numero % x == 0:
             return False
     return True
-
+    
+def logrec(array):
+    aux=[]
+    for i in array:
+        aux.append(math.log10(int(i)))
+    return aux
+    
+def ingerir():
+    
+    out_file = open("Practica2/primos.txt", "w") #Abre el txt 
+    out_file.write("Σ^(0,1) = {ε") #escribo info en el txt
+    cleartext(out_file, "Practica2/aux1.txt")
+    out_file.write("}\n")  
+    
+    out_file.write("Σ^E = {ε") #escribo info en el txt
+    array = cleartext(out_file, "Practica2/aux2.txt")
+    out_file.write("}\n")   
+    out_file.close()
+    
+    return array
+  
 def cleartext(out_file, file):
     array=[]
     in_file=open(file, "r")
@@ -48,49 +68,29 @@ def cleartext(out_file, file):
             in_file.close() #cierro el archivo
             break
         array.append(file_line)
-        out_file.write(","+file_line.strip('\n'))
+        out_file.write(" ,"+file_line.strip('\n'))
     os.remove(file)
     return array
-
-def ingerir():
-    
-    out_file = open("resultados.txt", "w") #Abre el txt 
-    out_file.write("L{e") #escribo info en el txt
-    
-    combinaciones=cleartext(out_file, "combinaciones.txt")
-    
-    out_file.write("}\n")
-    out_file.close() 
-
-    #graficar(combinaciones, unos, "Combinaciones posibles", "# de unos")
-    #graficar(combinaciones, largo, "Combinaciones posibles", "# de digitos")
-    
-    #graficar(combinaciones, logrec(unos), "Combinaciones posibles log10", "# de unos")
-    #graficar(combinaciones, logrec(largo), "Combinaciones posibles log10", "# de digitos")
-    
-def logrec(array):
-    aux=[]
-    for i in array:
-        aux.append(math.log10(int(i)))
-    return aux
     
 def graficar(x_axis, y_axis, titlex, titley):
     try:
         inicio = time.time()
-        fig, ax = plt.subplots()
-        ax.set_ylabel(titley)
-        ax.set_title(titlex)
-        ax.grid(False);
-        ax.plot(x_axis,y_axis)
-        ax.autoscale(enable=True, axis='both', tight=None)
-        plt.show()
+        ax = plt.subplot()
+        plt.xlabel(titlex)
+        plt.ylabel(titley)
+        plt.title(titlex + " y "+titley)
+        plt.autoscale(True, axis='x', tight=True)
+        ax.plot(x_axis, y_axis)
+        #plt.setp(ax.get_xticklabels(), rotation=40, ha='right', color='r', fontsize='5')
         fin = time.time()
+        plt.tight_layout()
+        plt.show()
         print("El tiempo de ejecucion para la grafica: "+titlex+", "+titley+" es: "+str(fin-inicio))
         
     except (KeyboardInterrupt, BufferError):
         fin = time.time()
+        plt.show()
         print("El tiempo de ejecucion para la grafica: "+titlex+", "+titley+" es: "+str(fin-inicio))
-        pass
 
 if __name__ == "__main__":
     while True:
@@ -105,51 +105,61 @@ if __name__ == "__main__":
             aux = int(input("Seleccione una opcion: "))
             
             if aux == 1:
-                n = int(input("\nIntroduce n, no mayor a 1000 ni menor a 0: "))
-                if 1000 < n < 0:
+                n = int(input("\nIntroduce n, no mayor a 2e7 ni menor a 2: "))
+                if 2e7 < n < 2:
                     print("Error, el numero introducido no es valido")
                     break
             
             elif aux ==2:
-                n = random.randrange(0,1000)
+                n = random.randrange(2,2e7)
             
             else:
                 print("Error, seleccione una opcion correcta")
                 break
             
-            check = True
-            count = 1
-            
-            filep = open("primos.txt", "w")
-            
+            file1 = open("Practica2/aux1.txt", "w")
+            file2 = open("Practica2/aux2.txt", "w")
+            lim = int(math.pow(2, n))
+            unos = []
+            largos = []
             inicio = time.time()
-            while check:
+            
+            for count in range(lim):
                 try:
-                    cadena = bina(count)
-                    if(primo(count)):
-                        filep.write(str(cadena.numero)+"--"+ str(count)+" \n")
-                    count += 1
-                    check = cadena.numero_de_1 != n
-                    del cadena
-                    
-                except BufferError:
-                    filep.close()
-                    exit()
-                    
-                except KeyboardInterrupt:
+                    if(primo(count)) and count != 0:
+                        cadena = bina(count)
+                        file1.write(str(cadena.numero)+" \n")
+                        file2.write(str(count)+" \n")
+                        unos.append(cadena.numero_de_1)
+                        largos.append(cadena.largo)
+                        del cadena
+                
+                except (KeyboardInterrupt,BufferError):
+                    file1.close()
+                    file2.close()
                     break
 
             fin = time.time()
-            filep.close()
+            file1.close()
+            file2.close()
             print("El tiempo de ejecucion para n = "+str(n)+" es: "+str(fin-inicio))
+            array = ingerir()
             del inicio
             del fin
             del aux
+            del lim
             del opc
             del n
             del count
-            ingerir()
             
+            graficar(array, largos, "Cadenas", "# de digitos")
+            graficar(array, unos, "Cadenas", "# de unos")
+            graficar(array, logrec(largos), "Cadenas", "log10 de # de digitos")
+            graficar(array, logrec(unos), "Cadenas", "log10 de # de unos")
+            
+            del largos
+            del unos
+            del array
             
         elif opc == 2:
             exit()
